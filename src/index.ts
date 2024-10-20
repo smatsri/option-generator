ba692dcf189f9ad7b4e19ae000ea218979ee8a93
Shai <smatsri@gmail.com>
2024-10-20 11:34:18 +0300

    asd

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import { option, toValue, map, none, some, bind } from "./option";
 import { pipe } from "./pipe";
 
+function div(a: number, b: number) {
+  if (b === 0) {
+    return none<number>();
+  }
+  return some(a / b);
+}
 
 const result = option(function* (_) {
-  let d = yield* _(some(1));
-  let a = yield* _(some(1));
-  let b = yield* _(some("123"));
-  //let c = yield* _(none<string>());
-  return b;
+  let a = yield* _(some(6));
+  let b = yield* _(some(0));
+  let c = yield* _(div(a, b));
+  return c;
 });
 
 const optionValue =
